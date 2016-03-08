e2264dbd106b3d1638005c2b28c9dd65ccd1f2b1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-08 20:47:14 +0000

    add sample for Ega
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.ims/trunk@84232 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/ims/views.py
+++ src/nti/app/products/ims/views.py
@@ -51,3 +51,43 @@ class SISPathAdapter(Contained):
 		self.request = request
 		self.__parent__ = parent
 		self.__name__ = SIS
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.externalization.internalization import read_body_as_external_object
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
+from nti.common.maps import CaseInsensitiveDict
+
+from nti.externalization.interfaces import LocatedExternalDict
+
+@view_config(name='LTIGrade')
+@view_config(name='lti_grade')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   context=LTIPathAdapter)
+class LTIGradeView(AbstractAuthenticatedView,
+				   ModeledContentUploadRequestUtilsMixin):
+
+	def readInput(self, value=None):
+		if self.request.body: # It's a post request
+			values = read_body_as_external_object(self.request)
+		else:
+			values = self.request.params # It's a get request
+		result = CaseInsensitiveDict(values)
+		return result
+
+	def __call__(self):
+		values = self.readInput()
+		print(values)
+		# TODO: do something w/ values
+		if True: # results are return
+			result = LocatedExternalDict()
+			return result
+		else: # otherwise
+			return hexc.HTTPOk()
