6940b488350739ed57142e4cfb2e41bafcdfcd64
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-09 02:44:18 +0000

    set descriptions
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.ims/trunk@106148 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,35 +4,35 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.app.products',
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app.products',
+    ],
 }
 
 setup(
-	name='nti.app.products.ims',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI IMS Product Integration",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app', 'nti.app.products'],
-	install_requires=[
-		'setuptools',
-		'nti.ims'
-	],
-	entry_points=entry_points
+    name='nti.app.products.ims',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI IMS Product Integration",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti', 'nti.app', 'nti.app.products'],
+    install_requires=[
+        'setuptools',
+        'nti.ims'
+    ],
+    entry_points=entry_points
 )
