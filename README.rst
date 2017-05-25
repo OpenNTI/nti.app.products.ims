242924e28052b7f7cdbeb5d9bfcbe2b0e9a016e3
Carlos Sanchez <papachoco@gmail.com>
2017-05-25 10:34:37 -0500

    add supporting files

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
+======================
+ nti.app.products.ims
+======================
 
+.. image:: https://travis-ci.org/NextThought/nti.app.products.ims.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.products.ims
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.products.ims/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.products.ims?branch=master
--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
 entry_points = {
     "z3c.autoinclude.plugin": [
         'target = nti.app.products',
     ],
 }
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='nti.app.products.ims',
-    version=VERSION,
+    version=_read('version.txt').strip(),
     author='Jason Madden',
     author_email='jason@nextthought.com',
-    description="NTI IMS Product Integration",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='pyramid preference',
+    description="NTI IMS Application",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='pyramid IMS',
     classifiers=[
-        'Framework :: Pyramid',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
@@ -27,12 +30,15 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
+    zip_safe=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     namespace_packages=['nti', 'nti.app', 'nti.app.products'],
     install_requires=[
         'setuptools',
-        'nti.ims'
+        'nti.ims',
     ],
-    entry_points=entry_points
+    entry_points=entry_points,
+    test_suite="nti.app.products.ims.tests",
 )
