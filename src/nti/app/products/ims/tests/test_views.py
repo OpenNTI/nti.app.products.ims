185abd3955c4a04049da5e420fcd77d9b426b748
chris.utz <chris.utz@nextthought.com>
2017-06-22 17:24:04 -0500

    add back in test case for bad credentials.

---
+++ src/nti/app/products/ims/tests/test_views.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+from hamcrest import ends_with
+from hamcrest import has_entries
+from hamcrest import has_entry
+
+from zope import component
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+import nti.dataserver.tests.mock_dataserver as mock_dataserver
+
+from nti.externalization.interfaces import StandardExternalFields
+
+from nti.ims.lti.interfaces import IOAuthConsumers
+
+from nti.ims.lti.oauth import OAuthConsumer
+
+ITEMS = StandardExternalFields.ITEMS
+ITEM_COUNT = StandardExternalFields.ITEM_COUNT
+
+from . import ConfiguringLayerMixin
+
+from lti.tool_config import ToolConfig
+from lti.tool_consumer import ToolConsumer
+
+class TestToolViews(ApplicationLayerTest):
+    """
+    We use the concrete launch tool as a test case for our general views
+    """
+
+    default_origin = 'http://janux.ou.edu'
+
+    def _register_consumer(self, key, secret, title=u'My Test Consumer'):
+        consumer = OAuthConsumer(key=key, secret=secret, title=title)
+        consumers = component.getUtility(IOAuthConsumers)
+        consumers[consumer.key] = consumer
+        return consumer
+
+    @WithSharedApplicationMockDS(users=False, testapp=True)
+    def test_config(self):
+        res = self.testapp.get('/dataserver2/IMS/LTI/TOOLS/launch/@@config')
+        conf = ToolConfig.create_from_xml(res.body)
+
+        assert_that(conf.title, is_('Launch NextThought'))
+        assert_that(conf.launch_url, is_('http://localhost/dataserver2/IMS/LTI/TOOLS/launch'))
+        assert_that(conf.secure_launch_url, is_('http://localhost/dataserver2/IMS/LTI/TOOLS/launch'))
+
+        assert_that(conf.extensions, has_entry('canvas.instructure.com',
+                                                    has_entries('privacy_level', 'Public',
+                                                                'oauth_compliant', 'true')) )
+
+    def _make_launch_request(self, key, secret):
+        tc = ToolConsumer(key, secret, params={
+                            'lti_message_type': 'basic-lti-launch-request',
+                            'lti_version': 'LTI-1.0',
+                            'resource_link_id': 'linkid',
+                            'launch_url': 'http://localhost/dataserver2/IMS/LTI/TOOLS/launch',
+                          })
+        return tc, tc.generate_launch_request()
+
+    @WithSharedApplicationMockDS(users=False, testapp=True)
+    def test_launch(self):
+        _, request = self._make_launch_request('does not', 'exist')
+
+        # A launch request with bad credentials 400s
+        result = self.testapp.post(request.url, headers=request.headers, params=request.body, status=400)
+
+        consumer = None
+        with mock_dataserver.mock_db_trans(self.ds):
+            consumer = self._register_consumer(u'foonextthoughtcom', u'supersecret')
+
+        _, request = self._make_launch_request(consumer.key, consumer.secret)
+        result = self.testapp.post(request.url, headers={}, params=request.body, status=303)
+        assert_that(result.location, ends_with('/NextThoughtWebApp/'))
+
+        with mock_dataserver.mock_db_trans(self.ds):
+            consumers = component.getUtility(IOAuthConsumers)
+            del consumers[consumer.key]
+
+
+
+
+
