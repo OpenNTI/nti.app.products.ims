f09f3a6c55a25833cd651196f4b9834b21853c3a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-02 12:39:40 -0500

    organize imports; pylint

--- src/nti/app/products/ims/provider.py
+++ src/nti/app/products/ims/provider.py
@@ -4,18 +4,19 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
-
-from zope import component
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from lti.utils import InvalidLTIRequestError
 
 from lti.tool_provider import ToolProvider as LTIToolProvider
 
+from zope import component
+
 from nti.app.products.ims.interfaces import IOAuthRequestValidator
+
+logger = __import__('logging').getLogger(__name__)
 
 
 class ToolProvider(LTIToolProvider):
